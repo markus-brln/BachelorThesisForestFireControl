4efe66f8fbb8ad5bcb0784402243099092e0a9f5
Thijs Visee <t.p.visee@student.rug.nl>
2021-03-11 10:15:56 +0100

    divided files

--- gui/Model/model.py
+++ gui/Model/model.py
@@ -4,13 +4,14 @@ class Model:
   # Other parameters can be added later
   ## Length: Grid size
   ## Agents: TODO determine: Number of agents or tuples of agent positions
-  def __init__(self, length: int, agents):
+  def __init__(self, length: int, agents, firesize = 1):
     self.size = length
     self.agents = agents
-    self.firebreaks
 
-    for agent in agents:
-      agent.model = self
+    ## Fire initialization
+    self.firesize = firesize
+    self.firepos = set()
+    self.centre = (self.size / 2, self.size / 2)
 
     self.startEpisode()           # Initialize episode
 
@@ -20,11 +21,18 @@ class Model:
     self.time = 0                 # Reset time
     self.terminal_state = False   # Restart so at initial state
 
-    self.centreX = int(self.size / 2)
-    self.centreY = int(self.size / 2)
     # Start fire in the middle of the map
-    self.firepos = set()
-    self.firepos.add((self.centreX, self.centreY))
+    self.firepos.clear()
+    self.firepos.add(self.centre)
+
+    idx = 1
+    while idx < self.firesize:
+     self.fire = list(self.firepos)
+     for pos in self.fire:
+      neighbours = self.getNeighbours(pos)
+      for neighbour in neighbours:
+       self.firepos.add(neighbour)
+     idx += 1
 
 
   ## Increment the time by one.
@@ -32,7 +40,6 @@ class Model:
   def time_step(self):
     self.time += 1                # Increment time
     self.expand_fire()            # Determine fire propagation
-    self.selected_squares = set()
 
 
   def select_square(self, position):
@@ -47,26 +54,41 @@ class Model:
     self.pos = position
     x = self.pos[0]
     y = self.pos[1]
-    if(x > 0 & x < self.size):
-      left = x - 1
-      right = x + 1
-    if(y > 0 & y < self.size):
-      top = y + 1
-      bottom = y - 1
+    self.left = x - 1
+    self.right = x + 1
+    self.top = y + 1
+    self.bottom = y - 1
     ## new list of neighbouring cells
-    neighbours = [(left, y), (x, top), (right, y), (x, bottom)]
-    return neighbours
+    neighbours = [(self.left, y), (x, self.top), (self.right, y), (x, self.bottom)]
+    for neighbour, (a, b) in enumerate(neighbours):
+     if self.position_in_bounds((a, b)):
+      return neighbours
+     else:
+      self.shut_down()
+
 
   ## currently stops when the fire reaches the edge of the map for simplicity but
   ## also as it makes it impossible for the agent to contain the fire
   def expand_fire(self):
-    fire = list(self.firepos)
-    for pos in fire:
+    self.fire = list(self.firepos)
+    print(self.fire)
+    for pos in self.fire:
       neighbours = self.getNeighbours(pos)
       for neighbour in neighbours:
         self.firepos.add(neighbour)
 
+  def position_in_bounds(self, position):
+    self.pos = position
+    self.x = self.pos[0]
+    self.y = self.pos[1]
+    # print("x: ", self.x, "y: ", self.y)
+    if ((self.x >= 0 & self.x <= self.size) & (self.y >= 0 & self.y <= self.size)):
+      return True
+    else:
+      return False
+
 
   ## TODO: e.g. save data and ensure proper exiting of program
   def shut_down(self):
-    print("Shutting Down")
+    print("Fire out of control!")
+    self.startEpisode()
--- gui/main.py
+++ gui/main.py
@@ -4,19 +4,30 @@
 import pygame
 
 from Model.model import Model
+from Model.agent import Agent
 from View.view import View
 from Controller.controller import Controller
 
+import random
+
+random.seed(0)
 
 pygame.init()  ## Initialize Pygame 
+
+def random_position(size):
+  return (random.randint(0, size), random.randint(0, size))
+
 
 def main():
   # Environment parameters
   size = 25
+  nr_of_agents = 5
   block_size_in_pixels = int(900 / size)
 
+  agents = [Agent(random_position(size)) for _ in range(nr_of_agents)]
+
   # Initialization
-  environment = Model(size, [])                   ## Initialize Environment
+  environment = Model(size, agents)            ## Initialize Environment
   view = View(environment, block_size_in_pixels)  ## Start View
   controller = Controller(environment, view)      ## Initialize Controller with model and view
 

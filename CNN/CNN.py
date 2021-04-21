a2f1885b7e7e0d4ef4bf1ad0b0fbde4ed0e543df
markus-brln <markusbauerlein@t-online.de>
2021-04-21 14:37:58 +0200

    pipeline works, just not learning anything

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -20,23 +20,19 @@ def build_model(input1_shape, input2_shape):
     model1.add(MaxPooling2D(pool_size=(2, 2)))
     model1.add(Dropout(0.25))
 
-    model1.add(Conv2D(filters=32, kernel_size=(3, 3), activation="relu", padding="same"))
-    model1.add(Conv2D(filters=32, kernel_size=(3, 3), activation="relu", padding="same"))
+    model1.add(Conv2D(filters=32, kernel_size=(2, 2), activation="relu", padding="same"))
     model1.add(MaxPooling2D(pool_size=(2, 2)))
     model1.add(Dropout(0.25))
-
+    model1.add(Dense(32, activation='relu'))                # 1D feature vector
     model1.add(Flatten())
-    model1.add(Dense(24, activation='sigmoid'))             # 1D feature vector
-
 
     inp2 = Input(input2_shape)
 
     model_concat = concatenate([model1.output, inp2], axis=1)
-    deconv = Dense(64, activation='relu')(model_concat)
-    deconv = keras.layers.Reshape((8, 8, 1))(deconv)        # TODO smoother way to upscale?
+    deconv = Dense(64, activation='relu')(model_concat)     # re-compress feature vector
+    deconv = keras.layers.Reshape((8, 8, 1))(deconv)
     deconv = Conv2DTranspose(64, (2, 2), padding='same')(deconv)
     deconv = keras.layers.Reshape((64, 64, 1))(deconv)      # 64x64 output, has to be translated to 255x255 again?
-
     model = Model(inputs=[model1.input, inp2], outputs=deconv)
 
     model.compile(loss='binary_crossentropy',               # because output pixels can have 0s or 1s
@@ -50,8 +46,8 @@ def load_model_and_predict():
     images, windinfo, outputs = load_data()
     model = keras.models.load_model("saved_models\\safetySafe")
     #X1 = images[0][np.newaxis, ...]                        # pretend as if there were multiple input pictures (verbose)
-    X1 = images[0:4]                                        # more clever way to write it down
-    X2 = windinfo[0:4]
+    X1 = images[50:60]                                        # more clever way to write it down
+    X2 = windinfo[50:60]
     results = model.predict([X1, X2])                        # outputs 61x61x1 for now
 
     orig_img = np.zeros((len(X1), 255, 255))
@@ -72,15 +68,12 @@ def load_model_and_predict():
 
 
 if __name__ == "__main__":
-    #load_model_and_predict()
-    #exit()
+    load_model_and_predict()
+    exit()
     images, windinfo, outputs = load_data()
 
-    print(outputs[0].shape)
     model = build_model(images[0].shape, windinfo[0].shape)
     print(model.summary())
-
-    #Y_train = np.random.randint(0, 2, (129, 64, 64, 1)) # TODO how to use the 255x255 image?
 
     model.fit([images, windinfo],                   # list of 2 inputs to model
               outputs,

3f7855e9196f1ab2fa6f0afdca13c92a3fb731e0
Thijs Visee <t.p.visee@student.rug.nl>
2021-04-24 15:34:18 +0200

    20 agents

--- gui/Model/utils.py
+++ gui/Model/utils.py
@@ -12,7 +12,7 @@ n_wind_speed_levels = 5
 
 # GLOBALS (documented when saving)
 size = 255  # environment size
-nr_of_agents = 30
+nr_of_agents = 20
 timeframe = 20  # timeframe in between setting new waypoints
 agentRadius = 70  # agents spawn in this radius around the fire (it's a box, not a circle, for now, see model.get_random_position())
 randseed = time.time()

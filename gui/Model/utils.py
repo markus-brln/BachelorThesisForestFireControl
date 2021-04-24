79e623039488e3a4ff9a866bf3f37b35b3f27501
markus-brln <markusbauerlein@t-online.de>
2021-04-24 08:55:28 +0200

    new branch

--- gui/Model/utils.py
+++ gui/Model/utils.py
@@ -14,9 +14,9 @@ n_wind_speed_levels = 5
 size = 255  # environment size
 nr_of_agents = 30
 timeframe = 20  # timeframe in between setting new waypoints
-agentRadius = 100  # agents spawn in this radius around the fire (it's a box, not a circle, for now, see model.get_random_position())
+agentRadius = 50  # agents spawn in this radius around the fire (it's a box, not a circle, for now, see model.get_random_position())
 randseed = time.time()
-fire_step_multiplicator = 3
+fire_step_multiplicator = 2
 
 # OTHER GLOBALS
 block_size_in_pixels = int(510 / size)

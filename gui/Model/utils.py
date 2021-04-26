646353da264afdac5a1bf6ebb82abefa1fc0a8fe
Hedopolis <j.w.j.hewlett@student.rug.nl>
2021-04-26 18:06:12 +0200

    things

--- gui/Model/utils.py
+++ gui/Model/utils.py
@@ -13,7 +13,7 @@ n_wind_speed_levels = 5
 # GLOBALS (documented when saving)
 size = 255  # environment size
 nr_of_agents = 5
-timeframe = 40  # timeframe in between setting new waypoints
+timeframe = 20  # timeframe in between setting new waypoints
 agentRadius = 70  # agents spawn in this radius around the fire (it's a box, not a circle, for now, see model.get_random_position())
 randseed = time.time()
 fire_step_multiplicator = 1

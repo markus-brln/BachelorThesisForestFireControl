f8e99fef47022961ac4b01ddb4847e611a2ec8f1
markus-brln <markusbauerlein@t-online.de>
2021-04-26 14:20:24 +0200

    CNN rewritten for driving waypoints

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -29,10 +29,17 @@ def build_model(input1_shape, input2_shape):
     inp2 = Input(input2_shape)
 
     model_concat = concatenate([model1.output, inp2], axis=1)
-    deconv = Dense(64, activation='relu')(model_concat)     # re-compress feature vector
-    deconv = keras.layers.Reshape((8, 8, 1))(deconv)
-    deconv = Conv2DTranspose(64, (2, 2), padding='same')(deconv)
-    deconv = keras.layers.Reshape((64, 64, 1))(deconv)      # 64x64 output, has to be translated to 255x255 again?
+    print(model_concat.shape)
+    deconv = Dense(8 * 8 * 2, activation='relu')(model_concat)     # re-compress feature vector
+    deconv = keras.layers.Reshape((8, 8, 2))(deconv)
+
+    # tutorial on deconv layer:
+    # https://machinelearningmastery.com/upsampling-and-transpose-convolution-layers-for-generative-adversarial-networks/
+    deconv = Conv2DTranspose(32, kernel_size=(2, 2), strides=(1, 1), padding='same')(deconv)
+    deconv = Conv2DTranspose(64, kernel_size=(2, 2), strides=(1, 1), padding='same')(deconv)
+    deconv = Conv2DTranspose(128, kernel_size=(2, 2), strides=(1, 1), padding='same')(deconv)
+
+    deconv = keras.layers.Reshape((64, 64, 2))(deconv)
     model = Model(inputs=[model1.input, inp2], outputs=deconv)
 
     model.compile(loss='binary_crossentropy',               # because output pixels can have 0s or 1s
@@ -46,10 +53,11 @@ def load_model_and_predict():
     images, windinfo, outputs = load_data()
     model = keras.models.load_model("saved_models\\safetySafe")
     #X1 = images[0][np.newaxis, ...]                        # pretend as if there were multiple input pictures (verbose)
-    X1 = images[50:60]                                        # more clever way to write it down
-    X2 = windinfo[50:60]
-    results = model.predict([X1, X2])                        # outputs 61x61x1 for now
+    X1 = images[:9]                                        # more clever way to write it down
+    X2 = windinfo[:9]
+    results = model.predict([X1, X2])                        # outputs 61x61x2
 
+    # translate the 5 channel input back to displayable images
     orig_img = np.zeros((len(X1), 255, 255))
     for i, image in enumerate(X1):
         for y, row in enumerate(image):
@@ -59,26 +67,39 @@ def load_model_and_predict():
                         orig_img[i][y][x] = idx
 
 
-
+    # display input images and the 2 waypoint output images (from 2 channels)
     for i in range(len(results)):
         plt.imshow(orig_img[i])
         plt.show()
-        plt.imshow(np.reshape(results[i], (64, 64)))
+        dig_img, drive_img = np.dsplit(results[i], 2)                  # depth split of 2 channel image
+        dig_img_real, drive_img_real = np.dsplit(outputs[i], 2)                  # depth split of 2 channel image
+        f, axarr = plt.subplots(2,2)
+        axarr[0,0].imshow(np.reshape(dig_img, newshape=(64, 64)))
+        axarr[0,0].set_title("dig waypoints image NN output")
+        axarr[0,1].imshow(np.reshape(drive_img, newshape=(64, 64)))
+        axarr[0,1].set_title("drive image NN output")
+        axarr[1,0].imshow(np.reshape(dig_img_real, newshape=(64, 64)))
+        axarr[1,0].set_title("dig image desired")
+        axarr[1,1].imshow(np.reshape(drive_img_real, newshape=(64, 64)))
+        axarr[1,1].set_title("drive image desired")
         plt.show()
 
 
 if __name__ == "__main__":
+    # TODO what should happen if we clicked on the same spot twice, i.e. only one waypoint recorded?
     load_model_and_predict()
     exit()
+
     images, windinfo, outputs = load_data()
 
     model = build_model(images[0].shape, windinfo[0].shape)
     print(model.summary())
 
+
     model.fit([images, windinfo],                   # list of 2 inputs to model
               outputs,
-              batch_size=16,
-              epochs=20,
+              batch_size=1,
+              epochs=500,
               shuffle=True)                         # mix data randomly
 
     save(model, "safetySafe")                       # utils

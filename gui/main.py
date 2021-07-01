6fb9bddda409c8e46a05d5e637ab3d53511730c2
markus <jh>
2021-07-01 12:41:46 +0200

    preparing for results

--- gui/main.py
+++ gui/main.py
@@ -12,10 +12,10 @@ from Model import utils
 def main():
   architecture_variants = ["xy", "angle", "box"]  # our 3 individual network output variants
   experiments = ["BASIC", "STOCHASTIC", "WINDONLY", "UNCERTAINONLY", "UNCERTAIN+WIND"]
-  variant = architecture_variants[1]
+  variant = architecture_variants[0]
   experiment = experiments[1]
-  n_NN_to_test = 10
-  n_runs_per_NN = 1
+  n_NN_to_test = 30
+  n_runs_per_NN = 100
 
   if len(sys.argv) > 1 and int(sys.argv[1]) < len(sys.argv):
       variant = architecture_variants[int(sys.argv[1])]

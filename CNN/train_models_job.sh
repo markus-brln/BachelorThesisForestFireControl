0de2ed8b9f1c738bd7af8e2c06b73d5c4af96534
Thijs Visee - S2982129 <t.p.visee@student.rug.nl>
2021-07-01 09:33:09 +0200

    about to start training

--- CNN/train_models_job.sh
+++ CNN/train_models_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash              
-#SBATCH --time=03:30:00                                                                                                                      
+#SBATCH --time=00:10:00                                                                                                                      
 #SBATCH --nodes=1                                                                                                                            
 #SBATCH --ntasks-per-node=1                                                                                                                  
 #SBATCH --mem=40G                                                                                                                            

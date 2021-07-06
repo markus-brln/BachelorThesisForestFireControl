0a6b99755486fc13feae0e4711bd931f6ccdddb8
markus <jh>
2021-07-06 10:54:27 +0200

    trying to load models

--- gui/Model/utils.py
+++ gui/Model/utils.py
@@ -22,39 +22,39 @@ digging_threshold = 0.5                                     # NN output > thresh
 apd = 10                                                    # agent_point_diameter when constructing the CNN input image
 
 def configure_globals(experiment_name):
-    global wind_on
-    global uncertain_spawn
-    global fire_step_multiplicator
-    global agentRadius
-    global experiment
-    experiment = experiment_name
+  global wind_on
+  global uncertain_spawn
+  global fire_step_multiplicator
+  global agentRadius
+  global experiment
+  experiment = experiment_name
 
-    if experiment_name == "BASIC":
-        wind_on = False
-        uncertain_spawn = 0
-    elif experiment_name == "STOCHASTIC":
-        wind_on = False
-        uncertain_spawn = 10
-    elif experiment_name == "WINDONLY":
-        wind_on = True
-        uncertain_spawn = 10
-        fire_step_multiplicator = 0.5
-        agentRadius = 90                                    # increased by 10 so first waypoint can be driving to the middle
-    elif experiment_name == "UNCERTAINONLY":
-        wind_on = False
-        uncertain_spawn = 30
-        agentRadius = 70
-    elif experiment_name == "UNCERTAIN+WIND":
-        wind_on = True
-        uncertain_spawn = 30
-        agentRadius = 70
-        fire_step_multiplicator = 0.5
+  if experiment_name == "BASIC":
+    wind_on = False
+    uncertain_spawn = 0
+  elif experiment_name == "STOCHASTIC":
+    wind_on = False
+    uncertain_spawn = 10
+  elif experiment_name == "WINDONLY":
+    wind_on = True
+    uncertain_spawn = 10
+    fire_step_multiplicator = 0.5
+    agentRadius = 90                                    # increased by 10 so first waypoint can be driving to the middle
+  elif experiment_name == "UNCERTAINONLY":
+    wind_on = False
+    uncertain_spawn = 30
+    agentRadius = 70
+  elif experiment_name == "UNCERTAIN+WIND":
+    wind_on = True
+    uncertain_spawn = 30
+    agentRadius = 70
+    fire_step_multiplicator = 0.5
 
 
 def rotate_point(origin, point, angle):
-    ox, oy = origin
-    px, py = point
-    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-    qy = oy + math.sin(angle) * (px - ox) - math.cos(angle) * (py - oy)
+  ox, oy = origin
+  px, py = point
+  qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+  qy = oy + math.sin(angle) * (px - ox) - math.cos(angle) * (py - oy)
 
-    return int(qx), int(qy)
+  return int(qx), int(qy)

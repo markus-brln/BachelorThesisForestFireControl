e74991f408bd14b0601b0603311f87d7640b5ff8
Thijs Visee - S2982129 <t.p.visee@student.rug.nl>
2021-07-09 09:21:46 +0200

    changes from peregrine

--- gui/results.sh
+++ gui/results.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=30G
 # send to GPU if needed #SBATCH --partition=gpu
-#SBATCH --job-name=data_translator_job
+#SBATCH --job-name=results
 
 # Load the module for R
 module load Python/3.7.4-GCCcore-8.3.0
@@ -18,9 +18,8 @@ module load matplotlib/3.1.1-foss-2019b-Python-3.7.4
 echo Overview of modules that are loaded
 module list
 
-# Run R using the code from mandelbrot.R
 echo starting main
-python main.py 2 1      # CHANGE architecture, experiment
+python main.py 2 4      # CHANGE architecture, experiment
 
 
 # execute with:
